0d33bc7c1801f32a4edd7e45947c25ead7da758b
Tomy Work <tomym87@gmail.com>
2020-04-15 17:00:28 -0500

    cambios del dia

--- JS/funCon.js
+++ JS/funCon.js
@@ -3,3 +3,18 @@ console.log(a.toFixed(5));
 
 var b = a.toString();
 console.log(b);
+
+console.log(a.toPrecision(4));
+
+function func(){
+    c = a+"f";
+    console.log(c);
+    if (c = "undefined"){
+        console.log("hay weeeey")
+    }
+    else{
+        console.log(c);
+        console.log("Error")
+    }
+}
+func();
