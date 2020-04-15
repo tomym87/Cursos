24955057c37ace59e19fcc4f39c0a0c1e37c7138
Tomy Work <tomym87@gmail.com>
2020-04-15 17:27:04 -0500

    termino dia dede kali

---
+++ JS/objString.js
@@ -0,0 +1,20 @@
+var a = new String ("Tomas Zarate");
+var b = "Tomas";
+
+console.log(a);
+console.log(a[0]);
+
+
+
+console.log(b[1]+b[2]);
+//En javascript la 
+
+console.log(a.toUpperCase());
+console.log(a.toLowerCase());
+
+var i = a.indexOf("m");
+console.log("La letra esta en : ", i);
+
+var nombre = a.substr(0,9);
+console.log(nombre);
+
