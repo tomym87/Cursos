2613a79aab830c4d433a99377249c9d85d097573
Tomy Work <tomym87@gmail.com>
2020-04-27 18:03:21 -0500

    toshiba

--- JS/json.js
+++ JS/json.js
@@ -1,6 +1,9 @@
 var objetoJS ={
     nombre:"Fernando",
-    edad:30
+    edad:30,
+    imprimir: function(){
+        console.log(this.nombre, this.edad);
+    }
 };
 
 console.log("ObjetoLIteral", objetoJS);
@@ -11,3 +14,6 @@ console.log(jsonString);
 
 var objetoDesdeJson = JSON.parse(jsonString);
 console.log(objetoDesdeJson);
+
+console.log(objetoDesdeJson.nombre);
+console.log(objetoDesdeJson.edad);
---
+++ JS/while.js
@@ -0,0 +1,32 @@
+/*
+var i = 0;
+
+while(i<10){
+    i++;
+
+    if(i=== 5){
+        //break; termina con la ejecucion selciclo
+        continue;//interrumpe laejecucion del ciclo pero continua 
+    }
+    console.log(i);
+
+}
+
+*/
+//Ciclo do wile primero ejecuta y leuego evalua
+
+
+
+var i = 0;
+
+do{
+    
+    i++;
+
+    if(i===5){
+        continue;
+    }
+    console.log(i);
+ }
+
+while(i < 10);
