d130e95129eb3a977e9a753489194327085f13b3
Tomy Work <tomym87@gmail.com>
2020-05-07 16:16:29 -0500

    jsmodernoArreglos

--- JSMODERNO/app.js
+++ JSMODERNO/app.js
@@ -1,2 +1,23 @@
-console.log("Pruebas");
+let videoJuegos = ["Mario 2", "Megaman, ChronoTiger"];
+
+console.log(videoJuegos);
+console.log({videoJuegos});
+
+
+
+
+let arregloCosas=[
+    true,
+    "Ignacio",
+    1+2,
+    function(){},
+    ()=>{},
+    {a: 1},
+    ["x", "Megaman", "Zero", "DR. Lighth"]
+
+];
+
+let ultimo = arregloCosas[6][arregloCosas[6].length-2]
+
+console.log(ultimo);
 
