58533dd671a15a351195715d203f3ed02c4cc1ed
Tomy Work <tomym87@gmail.com>
2020-05-15 18:32:03 -0500

    mas

--- JSMODERNO/02-BlackJack/assets/js/juego.js
+++ JSMODERNO/02-BlackJack/assets/js/juego.js
@@ -4,7 +4,7 @@ let deck        = [];
 const tipos     = ["C", "D", "H", "S"]
 const especiales = ["A", "J", "Q", "K"]
 
-
+//esta funcion crea una nueva baraja
 const crearDeck= () =>{
 
     for(let i = 2; i<=10; i++){
@@ -27,5 +27,36 @@ crearDeck();
 
 //Esta funcion me permite tomar una carta
 
+const pedirCarta=()=>{
+//codigo para que si me quedo son cartas no pueda seguir obteniendo cartas
+if (deck.length===0){
+    throw "NO hay cartas en el deck";
+}
+    const carta = deck.pop();
+    
+    console.log(deck);
+    console.log(carta);
+    return carta;
+}
+ //pedirCarta();
+const valorCarta = (carta) =>{
 
+    const valor = carta.substring (0, carta.length - 1);
+    
+    // let puntos = 0;
+    // if(isNaN(valor)){
+    //     puntos = (valor === 'A') ? 11 : 10;
+    // }else{
+    //     puntos = valor *1; //convertir un string a numero
+    // }
+    // console.log(puntos);
+
+    //resumiendo codigo
+    return (isNaN(valor)) ?
+            (valor === "A") ? 11 : 10
+            : valor * 1 ;
+
+}
+const valor = valorCarta(pedirCarta());
+console.log({valor});
 
